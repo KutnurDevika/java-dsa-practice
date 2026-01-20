958a180ab6e4682452714b2a01f1f76dc68451fa
KutnurDevika <devikakutnur@gmail.com>
2026-01-20 22:07:43 +0530

    Day 2: Add sum of array solution

---
+++ 01-arrays/SumArray.java
@@ -0,0 +1,14 @@
+// Problem: Sum of Array Elements
+// Approach: Linear traversal
+// Time: O(n), Space: O(1)
+
+public class SumArray {
+public static void main(String[] args) {
+ int[] arr = {10, 20, 30, 40};
+ int sum = 0;
+for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+System.out.println("Sum of elements: " + sum);
+    }
+}
