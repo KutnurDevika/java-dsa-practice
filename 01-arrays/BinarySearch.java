e274d0fad61ad713d6e0cf2b1506ca170a89ad25
KutnurDevika <devikakutnur@gmail.com>
2026-01-24 19:39:48 +0530

    Day 4: Implemented linear search, binary search, and prefix sum

---
+++ 01-arrays/BinarySearch.java
@@ -0,0 +1,37 @@
+// Problem: Binary Search
+// Approach: Divide and conquer
+// Time: O(log n), Space: O(1)
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 40;
+
+        int left = 0;
+        int right = arr.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                index = mid;
+                break;
+            } 
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
